d2b7f9a289bab6c41d05e07d3e21400e8b9d35b7
milton11 <milton1101@outlook.com>
2021-07-15 12:16:19 +0600

    commit

---
+++ Java/Dynamic/src/DynamicProgramming.java
@@ -0,0 +1,10 @@
+
+public class DynamicProgramming {
+
+	public static void main(String[] args) {
+		System.out.println("Start Real Dynamic Progrmming...with Java");
+		System.out.println("Hi I am Milton");
+
+	}
+
+}

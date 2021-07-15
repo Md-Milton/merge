34c7f2d9d72afcfed6746b7ec8e34d942680c602
milton11 <milton1101@outlook.com>
2021-07-15 12:03:29 +0600

    initial commit

---
+++ githubmerge/GitProjecct/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		 
+		System.out.println(addStrings("string1","string2"));
+	}
+
+	
+	 public static String addStrings(String num1, String num2) {
+	        
+	       int len1=num1.length()-1;
+	        int len2=num2.length()-1;
+	        
+	        StringBuilder sb=new StringBuilder();
+	        int sum=0,carry=0;
+	        while(len1>=0 || len2>=0) {
+	            int first=len1>=0?num1.charAt(len1)-'0':0;
+	            int second=len2>=0?num2.charAt(len2)-'0':0;
+	            sum=carry+first+second;
+	            if(sum<=9){
+	                sb.insert(0,sum);
+	                sum=0;
+	                carry=0;
+	            } else {
+	                sb.insert(0,sum%10);
+	                sum=0;
+	                carry=1;
+	            }
+	            len1--;
+	            len2--;
+	        }
+	    if(carry==1)sb.insert(0,"1");
+	    return sb.toString();
+	    }
+	
+}
